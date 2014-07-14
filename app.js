85ab26ed55a9906f7ca4112e092504e518152b6b
曾小斌 <137742685@qq.com>
2014-07-14 23:19:08 +0800

    init

---
+++ app.js
@@ -0,0 +1,21 @@
+var express=require('express');
+var path=require('path');
+var app = express();
+var server = require('http').Server(app);
+var io = require('socket.io')(server);
+
+server.listen(80);
+console.log("server was started!");
+
+app.use(express.static(path.join(__dirname)));
+
+app.get('/', function (req, res) {
+  res.sendfile(__dirname + '/app.html');
+});
+
+io.on('connection', function (socket) {
+  socket.emit('connectionok', "socket 连接成功");
+  socket.on('send', function (data) {
+  	socket.broadcast.emit('msgList', data);
+  });
+});
