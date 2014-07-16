d4b33ad755d30ff3467c9b2621d5a975e5fca3bf
xiaoice <137742685@qq.com>
2014-07-16 17:16:23 +0800

    xiaoice
    edit

---
+++ node.js
@@ -0,0 +1,41 @@
+var express=require('express');
+var path=require('path');
+var app = express();
+var server = require('http').Server(app);
+var io = require('socket.io')(server);
+
+server.listen(3000);
+console.log("server was started!");
+
+app.use(express.static(path.join(__dirname)));
+
+app.get('/app.html', function (req, res) {
+  res.sendfile(__dirname + '/app.html');
+});
+
+io.on('connection', function (socket) {
+  socket.emit('connectionok', "socket 连接成功");
+  var cache_name=undefined;
+
+  socket.on('send', function (data) {
+  	if(data.type==="login"){
+		cache_name=data.name;
+		console.log(data.name+data.text);
+  	}
+  	socket.broadcast.emit('msgList', data);
+  });
+
+  socket.on('disconnect', function (data) {
+  		var data={
+	  		type:"loginout"
+	  		,name:cache_name
+	  		,text:"离开了聊天室"
+	  		,time:new Date().toLocaleString()
+  		}
+  		cache_name=undefined;
+  		socket.broadcast.emit('msgList', data);
+  		console.log(data.name+data.text);
+  });
+
+
+});
