97ec469a92d3db0b861cce1a71e81392cae0c8ad
xiaoice <137742685@qq.com>
2014-07-23 10:00:37 +0800

    Revert "好友列表页面 添加socket.io提示上线、下线功能"
    This reverts commit c9d1c3f75e37d05fa675ae988b2823e422f3451f.

--- routers/index.js
+++ routers/index.js
@@ -1,79 +1,48 @@
 var express = require('express');
 var router = express.Router();
-var result = require('./result');
+
 
 //主模块，判断是否登录
 router.get('/', function (req, res) {
-  	if (!req.session.user) {
+	console.log(app.session);
+  	if (!req.session) {
     	return res.redirect('/login.html');
 	}else{
 		return res.redirect('/list.html');
 	}
 });
 
-
-//登录系统
 router.post('/login.do', function (req, res) {
-	var params=req.body;
-	var user={
-		openId:params.openId
-		,nickname:params.nickname
-		,gender:params.gender
-		,figureurl:params.figureurl
-		,figureurl_1:params.figureurl_1
-		,figureurl_2:params.figureurl_2
-		,figureurl_qq_1:params.figureurl_qq_1
-		,figureurl_qq_2:params.figureurl_qq_2
-		,province:params.province
-		,city:params.city
-	}
-	req.session.user=user;
-	req.session.save();
-	res.json(result.ok(req.session.user));
-});
-
-//退出登录
-router.get('/loginOut.do', function (req, res) {
-	if(req.session.user){
-		var user=JSON.parse(JSON.stringify(req.session.user));
-		req.session.destroy(function(err) {
-			res.json(result.ok(user));
-		})
-	}
+	console.log(req,req.body,req.params);
+    //res.render('login');
 });
 
 router.get('/login.html', function (req, res) {
-	if (req.session.user) {
-		if(req.query.type==="loginOut"){
-			res.render('login');
-		}else{
-    		res.render('list',JSON.parse(JSON.stringify(req.session.user)));
-		}
-	}else{
-		res.render('login');
-	}
+    res.render('login');
 });
 
 //监听是否登录-session
 router.use(function(req, res, next){
   res.locals.user = req.session.user;
   if (!req.session.user) {
-  	return res.redirect('/login.html');
+      return res.redirect('/login.html');
   }
-  //console.log('%s %s', req.method, req.url);
+  console.log('%s %s', req.method, req.url);
   next();
 });
 
+
 router.get('/reg.html', function (req, res) {
-    res.render('reg',req.session.user);
+    res.render('reg');
 });
 
 router.get('/list.html', function (req, res) {
-    res.render('list',JSON.parse(JSON.stringify(req.session.user)));
+    res.render('list');
 });
 
 router.get('/chaters.html', function (req, res) {
-    res.render('chaters',JSON.parse(JSON.stringify(req.session.user)));
+    res.render('chaters');
 });
+
 
 module.exports = router;
