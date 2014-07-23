9799a2b53cdeace1ca58737b630f1ae069ff6fd5
xiaoice <137742685@qq.com>
2014-07-23 10:28:23 +0800

    Revert "Revert "好友列表页面 添加socket.io提示上线、下线功能""
    This reverts commit 97ec469a92d3db0b861cce1a71e81392cae0c8ad.

---
+++ routers/result.js
@@ -0,0 +1,27 @@
+
+function result(recode,msg,data){
+	return new function(){
+		return {
+			recode:recode||0
+			,msg:msg||"FAIL"
+			,data:data||{}
+		}
+	};
+}
+
+exports.result=result;
+exports.fail=function(msg,data){
+	if(arguments.length===1&& typeof arguments[0]==="object"){
+		return result(0,"FAIL",arguments[0]||{})
+	}else{
+		return result(0,msg||"FAIL",data||{})
+	}
+}
+
+exports.ok=function(msg,data){
+	if(arguments.length===1&& typeof arguments[0]==="object"){
+		return result(1,"SUCCESS",arguments[0]||{})
+	}else{
+		return result(1,msg||"SUCCESS",data||{})
+	}
+}
